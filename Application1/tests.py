b1047872f59b1029b481ac5e7cf11b59fe4ce7e7
TimeTravel404eRroR <118643108+TimeTravel404eRroR@users.noreply.github.com>
2025-02-03 12:01:43 +0100

    tests NOK

--- Application1/tests.py
+++ Application1/tests.py
@@ -1,23 +1,24 @@
 from django.test import TestCase
+from django.db.utils import IntegrityError
 
 from .models import Item, Store
 # Create your tests here.
 
 class StoreItemTest(TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        print('trigger setUpClass')
-
-    @classmethod
-    def tearDownClass(cls):
-        print('trigger tearDownClass')
     
-    def setUp(self):
-        print('trigger setUp')
-    
-    def test_create_store(self):
-        self.assertFalse(False)
-
     def test_create_item(self):
-        self.assertTrue(True)
+        store = Store.objects.create(store_name='marche')
+        item1 = Item.objects.create(item_name='orange')
+        item1.save()
+        item_from_db = Item.objects.get(item_name='orange', store ='store')
+        self.assertIsNotNone(item_from_db)
+        self.assertEquals(item_from_db.item_name, 'orange')
+    
+    def test_create_item_error(self):
+        with self.assertRaises(IntegrityError):
+            Item.objects.create(item_name='orange')
+
+
+    def test_create_store_too_long_names(self):
+        with self.assertRaises(IntegrityError):
+            Store.objects.create(store_name='a'*100)
